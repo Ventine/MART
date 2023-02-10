5c938a04d9ed3bdf0ec9b0de4307ffc468bc164a
Ventine <jhondragon15@gmail.com>
2023-02-10 06:56:30 -0500

    version 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { TfiViewGrid, TfiCheckBox, TfiCalendar, TfiSaveAlt, TfiPowerOff, TfiMenu, TfiClose } from "react-icons/tfi";
+import { TfiViewGrid, TfiCheckBox, TfiCalendar, TfiSaveAlt, TfiPowerOff, TfiMenu, TfiClose, TfiBell , TfiAngleDown, TfiSearch,
+    TfiFlickrAlt, TfiSignal, TfiTime} from "react-icons/tfi";
 import React from 'react'
 import {useState} from "react";
 function App() {
@@ -8,12 +9,12 @@ function App() {
     }
   return (
     <div className="min-h-screen grid grid-cols-1 lg:grid-cols-6">
-        <div className={`fixed w-[80%] lg:w-full md:w-[50%] lg:static top-0 z-50 bg-white ${sidebar ? "-left-0" : "-left-full"} left-full w-full h-fulll overflow-y-scroll col-span-1 
-        p-6 border-r transition-all`}>
+        <div className={`fixed lg:static top-0 z-50 bg-white ${sidebar ? "-left-0" : "-left-full"} w-[74%] lg:w-full md:w-[45%] 
+        w-full h-full col-span-1 p-6 border-r transition-all`}>
             <div className="text-center p-6">
                 <h1 className="font-bold tracking-[5px] text-lime-800">MART</h1>
             </div>
-            <div className="flex flex-col justify-between h-[490px]">
+            <div className="flex flex-col justify-between h-[480px]">
                 <nav>
                     <ul>
                         <li>
@@ -60,13 +61,73 @@ function App() {
         <button onClick={handleSidebar} className="block lg:hidden absolute bottom-4 right-4 bg-lime-600 p-2 text-white
         rounded-full text-2xl">{sidebar ? <TfiClose /> : <TfiMenu />}</button>
         <div className="col-span-5">
-            <header>
-                <form>
-                    <div>
-                        <input type="text" className="bg-red-200" />
+            <header className="flex flex-col md:flex-row gap-4 items-center justify-between p-5 w-full">
+                <form className="w-full md:w-[40%] lg:w-[35%] order-1 md:order-none">
+                    <div className="relative ">
+                        <TfiSearch className="absolute left-2 top-3" />
+                        <input type="text" className="bg-lime-50 py-2 pl-9 pr-6 outline-none rounded-lg w-full"  placeholder="Buscar horario" />
                     </div>
                 </form>
+                <nav className="w-full md:w-[60%] lg:w-[65%] flex justify-center md:justify-end">
+                    <ul className="flex items-center gap-4">
+                        <li>
+                            <a href="#hom2" className="relative">
+                                <TfiFlickrAlt className="absolute -right-1 -top-1 text-xs text-red-500" />
+                                <TfiBell className="text-xl"  />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#hom2" className="flex items-center gap-2">
+                                Jhon Torres
+                                <TfiAngleDown />
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
             </header>
+            <div className="p-9 bg-lime-50">
+                <div className="mb-6">
+                    <h1 className="text-2xl font-semibold">Area de trabajo</h1>
+                </div>
+                <div className="grid grid-cols-4 gap-3 items-center mb-5 ">
+                    <form className="col-span-2">
+                        <div className="relative ">
+                            <TfiSearch className="absolute left-2 top-3 text-lime-600" />
+                            <input type="text" className="bg-white-50 py-2 pl-9 pr-6 outline-none rounded-lg w-full"  placeholder="Buscar tarea" />
+                        </div>
+                    </form>
+                    <form className="col-span-1">
+                        <div className="relative ">
+                            <TfiSignal className="absolute left-2 top-3 text-lime-600" />
+                            <select className="bg-white-50 py-2 pl-9 pr-6 outline-none rounded-lg w-full text-gray-400">
+                                <option >Prioridad</option>
+                            </select>
+                        </div>
+                    </form>
+                    <form className="col-span-1">
+                        <div className="relative ">
+                            <TfiTime className="absolute left-2 top-3 text-lime-600" />
+                            <input type="number" className="bg-white-50 py-2 pl-9 pr-6 outline-none rounded-lg w-full" step={30}  placeholder="Tiempo" />
+                            {/*<span className="absolute text-sm right-3 top-1 bg-lime-500 text-white rounded-full py-1 px-[10px]">3</span>*/}
+                        </div>
+                    </form>
+                </div>
+                <div className="flex items-center gap-3">
+                    <span className="bg-white flex items-center gap-2 py-3 px-5 pl-3 pr-4 rounded-full">
+                        <button className="bg-lime-100 p-1 rounded-full text-lime-600 text-xs"><TfiClose /></button>
+                        <span className="text-gray-600">Horario</span>
+                    </span>
+                    <span className="bg-white flex items-center gap-2 py-3 px-5 pl-3 pr-4 rounded-full">
+                        <button className="bg-lime-100 p-1 rounded-full text-lime-600 text-xs"><TfiClose /></button>
+                        <span className="text-gray-600">30</span>
+                    </span>
+                    <span className="bg-white flex items-center gap-2 py-3 px-5 pl-3 pr-4 rounded-full">
+                        <button className="bg-lime-100 p-1 rounded-full text-lime-600 text-xs"><TfiClose /></button>
+                        <span className="text-gray-600">Ejercicio</span>
+                    </span>
+                     <button className="text-lime-500 ml-4">Borrar busquedas</button>
+                </div>
+            </div>
         </div>
     </div>
   )
