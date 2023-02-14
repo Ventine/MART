68a40552590a6074a295d1217df51a567da97075
Ventine <jhondragon15@gmail.com>
2023-02-14 12:26:00 -0500

    VErsion 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,23 @@
-import { TfiViewGrid, TfiCheckBox, TfiCalendar, TfiSaveAlt, TfiPowerOff, TfiMenu, TfiClose, TfiBell , TfiAngleDown, TfiSearch,
-    TfiFlickrAlt, TfiSignal, TfiTime, TfiBasketball, TfiBook} from "react-icons/tfi";
 import React from 'react'
-import {useState} from "react";
-import InicioSesion from "./componentes/InicioSesion.jsx";
-import Registrarse from "./componentes/Registrarse.jsx";
-import Premium from "./componentes/Premium.jsx";
 import AreaDeTrabajo from "./componentes/AreaDeTrabajo.jsx";
 import HeaderSup from "./componentes/HeaderSup.jsx";
 import NavBar from "./componentes/NavBar.jsx";
 import {Route, Routes} from "react-router-dom";
 import Tareas from "./componentes/Tareas.jsx";
+
 function App() {
-    const [sidebar, setSidebar] = useState(false);
-    const handleSidebar = () =>{
-        setSidebar(!sidebar)
-    }
-  return (
-     <div className="min-h-screen grid grid-cols-1 lg:grid-cols-6">      {/*Pagina principal*/}
-        {/*Menu derecho*/}
-        <div className={`w-[62%] fixed lg:static top-0 z-50 bg-white  ${sidebar ? "-left-0" : "-left-full"} lg:w-full md:w-[45%] 
-        w-full h-full col-span-1 p-6 border-r transition-all`}>
-            <div className="flex flex-col justify-between h-[480px]">
-                <NavBar />
+    return (
+        <div className="min-h-screen grid grid-cols-1 lg:grid-cols-6">      {/*Pagina principal*/}
+            <NavBar/>
+            <div className="col-span-5">
+                <HeaderSup/>
+                <Routes>
+                    <Route path="/" element={<AreaDeTrabajo/>}/>
+                    <Route path="tareas" element={<Tareas/>}/>
+                </Routes>
             </div>
         </div>
-        <button onClick={handleSidebar} className="block lg:hidden fixed bottom-4 right-4 bg-lime-600 p-2 text-white
-        rounded-full text-2xl z-40">{sidebar ? <TfiClose /> : <TfiMenu />}</button>
-        <div className="col-span-5">
-            <HeaderSup />
-            <Routes>
-                <Route path="/" element={<AreaDeTrabajo /> } />
-                <Route path="tareas" element={<Tareas /> } />
-            </Routes>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default App
--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -1,57 +1,71 @@
-import {TfiCalendar, TfiCheckBox, TfiSaveAlt, TfiViewGrid} from "react-icons/tfi";
+import {TfiCalendar, TfiCheckBox, TfiClose, TfiMenu, TfiSaveAlt, TfiViewGrid} from "react-icons/tfi";
 import {Link, Outlet} from "react-router-dom";
 import Premium from "./Premium.jsx";
 import InicioSesion from "./InicioSesion.jsx";
 import Registrarse from "./Registrarse.jsx";
+import React, {useState} from "react";
 
-function NavBar(){
-    return(
+function NavBar() {
+    const [sidebar, setSidebar] = useState(false);
+    const handleSidebar = () => {
+        setSidebar(!sidebar)
+    }
+    return (
         <div>
-            <div className="text-center p-6">
-                <h1 className="font-bold tracking-[5px] text-lime-800">MART</h1>
-            </div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/" className="flex items-center gap-1.5 hover:bg-lime-600 p-3 text-gray-400 hover:text-white rounded-lg
+            {/*Menu derecho*/}
+            <div className={`w-[62%] fixed lg:static top-0 z-50 bg-white  ${sidebar ? "-left-0" : "-left-full"} lg:w-full md:w-[45%] 
+        w-full h-full col-span-1 p-6 border-r transition-all`}>
+                <div className="flex flex-col justify-between h-[480px]">
+                    <div className="text-center p-6">
+                        <h1 className="font-bold tracking-[5px] text-lime-800">MART</h1>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/" className="flex items-center gap-1.5 hover:bg-lime-600 p-3 text-gray-400 hover:text-white rounded-lg
                         transition-colors font-semibold">
-                            <TfiViewGrid />
-                            Tablero
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/tareas" className="flex items-center gap-2 hover:bg-lime-600 p-4 text-gray-400 hover:text-white rounded-lg
+                                    <TfiViewGrid/>
+                                    Tablero
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/tareas" className="flex items-center gap-2 hover:bg-lime-600 p-4 text-gray-400 hover:text-white rounded-lg
                         transition-colors font-semibold">
-                            <TfiCheckBox />
-                            Tareas
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/horario" className="flex items-center gap-2 hover:bg-lime-600 p-4 text-gray-400 hover:text-white rounded-lg
+                                    <TfiCheckBox/>
+                                    Tareas
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/horario" className="flex items-center gap-2 hover:bg-lime-600 p-4 text-gray-400 hover:text-white rounded-lg
                         transition-colors font-semibold">
-                            <TfiCalendar />
-                            Horario
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/historial" className="flex items-center gap-2 hover:bg-lime-600 p-4 text-gray-400 hover:text-white rounded-lg
+                                    <TfiCalendar/>
+                                    Horario
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/historial" className="flex items-center gap-2 hover:bg-lime-600 p-4 text-gray-400 hover:text-white rounded-lg
                         transition-colors font-semibold">
-                            <TfiSaveAlt />
-                            Historial
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-            <div className="flex flex-col gap-3 mt-10">
-                <div className="bg-lime-50 p-3 flex flex-col gap-3 rounded-3xl">
-                    <h3 className="text-xl text-center">Hazte Premium</h3>
-                    <p className="text-gray-500 text-center">Accede a muchas funcionalidades haciendote premium.</p>
-                    <Premium />
+                                    <TfiSaveAlt/>
+                                    Historial
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className="flex flex-col gap-3 mt-10">
+                        <div className="bg-lime-50 p-3 flex flex-col gap-3 rounded-3xl">
+                            <h3 className="text-xl text-center">Hazte Premium</h3>
+                            <p className="text-gray-500 text-center">Accede a muchas funcionalidades haciendote
+                                premium.</p>
+                            <Premium/>
+                        </div>
+                        <InicioSesion/>
+                        <Registrarse/>
+                    </div>
+                    <Outlet/>
                 </div>
-                <InicioSesion />
-                <Registrarse />
             </div>
-            <Outlet />
+            <button onClick={handleSidebar} className="block lg:hidden fixed bottom-4 right-4 bg-lime-600 p-2 text-white
+        rounded-full text-2xl z-40">{sidebar ? <TfiClose/> : <TfiMenu/>}</button>
         </div>
     )
 }
